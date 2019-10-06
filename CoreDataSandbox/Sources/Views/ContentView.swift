9a165d1340feeb92483f5578e5bc5eeafb7f58fe
Shun Kuroda <kuroyam1109@gmail.com>
2019-10-06 10:47:33 +0900

    Initial commit.

---
+++ CoreDataSandbox/Sources/Views/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  CoreDataSandbox
+//
+//  Created by Shun Kuroda on 2019/10/06.
+//  Copyright © 2019 Shun Kuroda. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
